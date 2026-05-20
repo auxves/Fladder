a202c19d798f9800e41d3e217189d265a53dcc4e
auxves <me@auxves.dev>
2026-05-19 22:00:28 -0700

    ios: add build script

---
+++ build_ios.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+flutter build ios --flavor production --no-codesign
+
+codesign -s - -f build/ios/iphoneos/Runner.app
+codesign -s - -f build/ios/iphoneos/Runner.app/Frameworks/*
+
+cd build/ios/iphoneos
+mkdir Payload
+mv Runner.app Payload/
+zip -qr Fladder.ipa Payload
+
+echo "IPA built at $(realpath Fladder.ipa)"
